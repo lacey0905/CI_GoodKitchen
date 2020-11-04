dd99b738c43a5df5aeb994769bf02d44da0e52d9
username <user@email.mail>
2020-11-04 16:37:20 +0900

    서울시 api 데이터와 카카오맵 연동 및 주소로 마커 생성

---
+++ application/controllers/map.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Map extends CI_Controller {
+	public function index()
+	{
+        $MAP_LIST_LENGTH = 50;
+        $url = 'http://openAPI.seoul.go.kr:8088/424b627a726c616339396d69555078/json/ListPriceModelStoreService/1/'.$MAP_LIST_LENGTH.'/001'; /*URL*/
+        $data =file_get_contents($url);
+
+        $json_object = json_decode( $data, false );
+        $json_data = json_encode($json_object, JSON_UNESCAPED_UNICODE);
+        echo $json_data;
+    }
+}
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,8 +1,68 @@
 <?php
 	
-	$url = 'http://openAPI.seoul.go.kr:8088/424b627a726c616339396d69555078/xml/ListPriceModelStoreService/1/5/'; /*URL*/
-	$data =file_get_contents($url);
-	$xml = simplexml_load_string($data);
-	echo $xml->row[0]->SH_NAME[0];
+
+	// echo "test";
+	
 
 ?>
+
+<div id="map" style="width:50%;height:100%;"></div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=0cdf332fff818cba293b2ea58cf8d7ca&libraries=services"></script>
+<script>
+
+	var addrs = [];
+
+	$.ajax({
+		// url: 'http://openAPI.seoul.go.kr:8088/424b627a726c616339396d69555078/json/ListPriceModelStoreService/1/10/001',
+		url: 'map',
+		cache: false,                     //사용자캐시를 사용할 것인가.
+		dataType: "json",                  //서버로부터 받을 것으로 예상되는 데이터 타입.
+		success: function(data){          //ajax요청을 통해 반환되는 데이터 data.
+			var row = data.ListPriceModelStoreService.row;
+			for(var i=0; i<row.length; i++) {
+				maxMarker(row[i].SH_ADDR, row[i].SH_NAME);
+			}                          
+		}
+	});
+
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+		mapOption = {
+			center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+			level: 3 // 지도의 확대 레벨
+		};  
+
+	// 지도를 생성합니다    
+	var map = new kakao.maps.Map(mapContainer, mapOption); 
+
+	function maxMarker(_addr, _name) {
+		// 주소-좌표 변환 객체를 생성합니다
+		var geocoder = new kakao.maps.services.Geocoder();
+
+		// 주소로 좌표를 검색합니다
+		geocoder.addressSearch(_addr, function(result, status) {
+
+			// 정상적으로 검색이 완료됐으면 
+			if (status === kakao.maps.services.Status.OK) {
+
+				var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+				// 결과값으로 받은 위치를 마커로 표시합니다
+				var marker = new kakao.maps.Marker({
+					map: map,
+					position: coords
+				});
+
+				// 인포윈도우로 장소에 대한 설명을 표시합니다
+				var infowindow = new kakao.maps.InfoWindow({
+					content: '<div style="width:150px;text-align:center;padding:6px 0;">' + _name + '</div>'
+				});
+				infowindow.open(map, marker);
+
+				// 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+				map.setCenter(coords);
+			} 
+		});   
+	}
+
+</script>
